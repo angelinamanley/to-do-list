840ae98f4e9d69c0b20896af0a9d227dbe589a48
github username <github email address>
2019-10-09 12:14:19 +0100

    bonus deliverables done

--- src/App.js
+++ src/App.js
@@ -1,73 +1,83 @@
-import React from 'react';
-import './App.css';
-import { CATEGORIES } from './data'
-import Categories from './Categories.js'
-import Tasks from './Tasks.js'
-
-
+import React from "react";
+import "./App.css";
+import { CATEGORIES } from "./data";
+import Categories from "./Categories.js";
+import Tasks from "./Tasks.js";
 
 class App extends React.Component {
-
   state = {
-    category : "All", 
+    category: "All",
     tasks: [
       {
-        text: 'Buy rice',
-        category: 'Food'
+        text: "Buy rice",
+        category: "Food"
       },
       {
-        text: 'Save a tenner',
-        category: 'Money'
+        text: "Save a tenner",
+        category: "Money"
       },
       {
-        text: 'Build a todo app',
-        category: 'Code'
+        text: "Build a todo app",
+        category: "Code"
       },
       {
-        text: 'Build todo API',
-        category: 'Code'
+        text: "Build todo API",
+        category: "Code"
       },
       {
-        text: 'Get an ISA',
-        category: 'Money'
+        text: "Get an ISA",
+        category: "Money"
       },
       {
-        text: 'Cook rice',
-        category: 'Food'
+        text: "Cook rice",
+        category: "Food"
       },
       {
-        text: 'Tidy house',
-        category: 'Misc'
+        text: "Tidy house",
+        category: "Misc"
       }
     ]
-  }
+  };
 
+  addNewTask = object => {
+    console.log(object)
+    this.setState({
+      category: this.state.category,
+      tasks: [...this.state.tasks, object]
 
+    });
+  };
 
   filterCategory = () => {
     if (this.state.category === "All") {
-      return this.state.tasks
+      return this.state.tasks;
     } else {
-    return this.state.tasks.filter(task => task.category === this.state.category)
-  }}
+      return this.state.tasks.filter(
+        task => task.category === this.state.category
+      );
+    }
+  };
 
-  changeCategoryState = (name) => {
-    this.setState(
-      { tasks: [...this.state.tasks],
-        category : name}
-    )
-  } 
+  changeCategoryState = name => {
+    this.setState({ text: [...this.state.tasks], category: name });
+  };
 
   render() {
     return (
       <div className="App">
         <h2>My tasks</h2>
-        <Categories catArray = {CATEGORIES} changeCategoryState={this.changeCategoryState}/>
-        <Tasks  taskArray={this.filterCategory()}/>
+        <Categories
+          catArray={CATEGORIES}
+          changeCategoryState={this.changeCategoryState}
+        />
+        <Tasks
+          addTask={this.addNewTask}
+          catArray={CATEGORIES}
+          taskArray={this.filterCategory()}
+        />
       </div>
     );
   }
 }
-
 
 export default App;
--- src/Tasks.js
+++ src/Tasks.js
@@ -1,14 +1,44 @@
 import React from 'react'
+import { createCoverageSummary } from 'istanbul-lib-coverage'
 
 export default class Tasks extends React.Component {
 
+    state = {
+        newTask : "",
+        newCat : ""
+    }
 
+  
+    handleClick = (event) => {
+        event.target.parentNode.remove()
+    }
+
+    handleFormSubmit = (event) => {
+        event.preventDefault()
+        let newTask = {
+            text: event.target.children[0].value,
+        category: event.target.children[1].value}
+        this.props.addTask(newTask)
+}
+
+    addTasktoArray = (task) => {
+        let newTasksArray = [...this.props.taskArray]
+        newTasksArray.push(task)
+        return newTasksArray
+    }
 
     render() {
         return (
             <div className="tasks">
                 <h5>Tasks</h5>
-                {this.props.taskArray.map(task=> <div className="task"><div className="label">{task.category}</div><div className="text">{task.text}</div></div>)}
+                <form class="new-task-form" onSubmit={this.handleFormSubmit}>
+                  <input placeholder="New task details" type="text"></input>
+                  <select>
+                {this.props.catArray.map(category => <option>{category}</option>)}
+                  </select>
+                  <input type="submit" value="Add Task" />
+              </form>
+                {this.props.taskArray.map(task=> <div className="task"><div className="label">{task.category}</div><div className="text">{task.text}</div><button onClick={this.handleClick} className="delete">X</button></div>)}
             </div>
         )
       }
